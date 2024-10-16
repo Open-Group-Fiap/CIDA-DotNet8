788973c7ddf406b2f5b362ef8bd5dd9bb5b9c81a
oleoz1n <leopity0@gmail.com>
2024-10-16 09:05:17 -0300

    Feat: Add Units Tests (Only Start)

--- CIDA.Api/Program.cs
+++ CIDA.Api/Program.cs
@@ -5,78 +5,86 @@ using Cida.Data;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Azure;
 using Microsoft.OpenApi.Models;
-using Newtonsoft.Json;
 using Swashbuckle.AspNetCore.Filters;
 
-var builder = WebApplication.CreateBuilder(args);
-
-#region Database
-
-builder.Services.AddDbContext<CidaDbContext>(options =>
+namespace CIDA.Api
 {
-    options.UseOracle(builder.Configuration.GetConnectionString("FiapOracleConnection"));
-});
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
 
-#endregion
+            var builder = WebApplication.CreateBuilder(args);
 
-#region Azure Blob Storage
+            #region Database
+
+            builder.Services.AddDbContext<CidaDbContext>(options =>
+            {
+                options.UseOracle(builder.Configuration.GetConnectionString("FiapOracleConnection"));
+            });
+
+            #endregion
+
+            #region Azure Blob Storage
 
 // add singleton azure blob service
-builder.Services.AddSingleton(x =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("AzureStorage");
-    return new BlobServiceClient(connectionString);
-});
+            builder.Services.AddSingleton(x =>
+            {
+                var connectionString = builder.Configuration.GetConnectionString("AzureStorage");
+                return new BlobServiceClient(connectionString);
+            });
 
-#endregion
+            #endregion
 
 // add HttpClient
-builder.Services.AddHttpClient();
+            builder.Services.AddHttpClient();
 
 // HealthChecks
-builder.Services.ConfigureHealthChecks(builder.Configuration);
+            builder.Services.ConfigureHealthChecks(builder.Configuration);
 
-#region Swagger Configuration
+            #region Swagger Configuration
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(options =>
-{
-    options.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Version = "v1",
-        Title = "Cida API",
-        Description = "Uma API para gerenciamento do banco de dados da CIDA",
-    });
-    options.ExampleFilters();
-});
-builder.Services.AddSwaggerExamplesFromAssemblyOf<Program>();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Cida API",
+                    Description = "Uma API para gerenciamento do banco de dados da CIDA",
+                });
+                options.ExampleFilters();
+            });
+            builder.Services.AddSwaggerExamplesFromAssemblyOf<Program>();
 
-#endregion
+            #endregion
 
-var app = builder.Build();
+            var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(options =>
-    {
-        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
-        options.RoutePrefix = string.Empty;
-    });
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                    options.RoutePrefix = string.Empty;
+                });
+            }
+
+            app.MapHealthChecks("/api/health", new HealthCheckOptions()
+            {
+                Predicate = _ => true,
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
+            app.UseHealthChecksUI(options => { options.UIPath = "/healthcheck-ui"; });
+
+            app.MapUsuarioEndpoints();
+            app.MapResumoEndpoints();
+            app.MapInsightEndpoints();
+            app.MapLoginEndpoints();
+            app.MapArquivoEndpoints();
+            app.Run();
+        }
+    }
 }
-
-app.MapHealthChecks("/api/health", new HealthCheckOptions()
-{
-    Predicate = _ => true,
-    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-});
-app.UseHealthChecksUI(options => { options.UIPath = "/healthcheck-ui"; });
-
-app.MapUsuarioEndpoints();
-app.MapResumoEndpoints();
-app.MapInsightEndpoints();
-app.MapLoginEndpoints();
-app.MapArquivoEndpoints();
-app.Run();
---
+++ CIDA.Tests/UsuarioApiTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Net.Http.Json;
+using CIDA.Domain.Entities;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace CIDA.Tests;
+using Xunit;
+
+public class UsuarioApiTests : IClassFixture<WebApplicationFactory<CIDA.Api.Program>>
+{
+    private readonly HttpClient _client;
+
+    public UsuarioApiTests(WebApplicationFactory<CIDA.Api.Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+    
+    [Fact]
+    public async Task GetUsuarioById_ReturnsUsuario()
+    {
+        // Act
+        var response = await _client.GetAsync("/usuario/21");
+
+        // Assert
+        response.EnsureSuccessStatusCode();
+        var usuario = await response.Content.ReadFromJsonAsync<Usuario>();
+
+        Assert.NotNull(usuario);
+    }
+    
+    
+}
