b9663a4ed6f511db71a1762de949aacc8254c7c6
oleoz1n <leopity0@gmail.com>
2024-10-28 11:23:37 -0300

    Feat: add IA api tests

---
+++ CIDA.Tests/IAApiTests.cs
@@ -0,0 +1,44 @@
+﻿using CIDA.Api;
+using CIDA.Api.Models.Requests;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Net.Http.Json;
+
+
+namespace CIDA.Tests;
+
+[Collection("Api Test Collection")]
+public class IAApiTests
+{
+
+    private readonly HttpClient _client;
+
+    public IAApiTests(WebApplicationFactory<Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async void PostPrevisao_ReturnsPrevisao() {
+        
+        // Arrange
+        var previsao = new PrevisaoRequest("Verão", "Sul");
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/previsao", previsao);
+
+        // Assert
+        Assert.NotNull(response);
+    }
+
+    [Fact]
+    public async void GetMetrics_ReturnsMetrics()
+    {
+        // Act
+        var response = await _client.GetAsync("previsao/metrics");
+
+        // Assert
+        Assert.NotNull(response);
+        
+    }
+}
