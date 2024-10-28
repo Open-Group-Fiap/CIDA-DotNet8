71323587db8db1a51417e5211c911080277c6a08
oleoz1n <leopity0@gmail.com>
2024-10-28 11:32:06 -0300

    feat: add bad requsts tests

--- CIDA.Tests/IAApiTests.cs
+++ CIDA.Tests/IAApiTests.cs
@@ -2,6 +2,7 @@
 using CIDA.Api.Models.Requests;
 using Microsoft.AspNetCore.Mvc.Testing;
 using System;
+using System.Net;
 using System.Net.Http.Json;
 
 
@@ -29,6 +30,47 @@ public class IAApiTests
 
         // Assert
         Assert.NotNull(response);
+    }
+
+    
+
+    [Fact]
+    public async void PostPrevisao_ReturnsBadRequest_WhenEstacaoIsInvalid()
+    {
+        // Arrange
+        var previsao = new PrevisaoRequest("Verão2", "Sul");
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/previsao", previsao);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Estação inválida. Use: Verão, Inverno, Primavera ou Outono", content.Trim('"'));
+    }
+    [Fact]
+    public async void PostPrevisao_ReturnsBadRequest_WhenRegiaoIsInvalid()
+    {
+        // Arrange
+        var previsao = new PrevisaoRequest("Verão", "Sul2");
+
+        // Act
+        var response = await _client.PostAsJsonAsync("/previsao", previsao);
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
+        var content = await response.Content.ReadAsStringAsync();
+        Assert.Equal("Região inválida. Use: Sul, Sudeste, Centro-Oeste, Norte ou Nordeste", content.Trim('"'));
+    }
+
+    [Fact]
+    public async void PostPrevisao_ReturnsBadRequest_WhenSendRandomJson()
+    {
+        // Act
+        var response = await _client.PostAsJsonAsync("/previsao", new {});
+
+        // Assert
+        Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
     }
 
     [Fact]
