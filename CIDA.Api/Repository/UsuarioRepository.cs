0215042607f7caad9705881c75d09c7b221f3324
oleoz1n <leopity0@gmail.com>
2024-11-01 20:19:39 -0300

    Feat: Create a UsuarioRepository.cs and make Integration and Abrangente tests

---
+++ CIDA.Api/Repository/UsuarioRepository.cs
@@ -0,0 +1,61 @@
+﻿using Cida.Data;
+using CIDA.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CIDA.Api.Repository;
+
+public class UsuarioRepository
+{
+    private readonly CidaDbContext _context;
+
+    public UsuarioRepository(CidaDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Usuario> GetUsuarioAsync(int id)
+    {
+        return await _context.Usuarios.FindAsync(id);
+    }
+
+    public async Task<IEnumerable<Usuario>> GetUsuariosByNomeAsync(string nome)
+    {
+        return await _context.Usuarios.Where(x => x.Nome == nome).ToListAsync();
+    }
+
+    public async Task<Usuario> GetUsuarioByNumDocumentoAsync(string numDocumento)
+    {
+        return await _context.Usuarios.FirstOrDefaultAsync(x => x.NumDocumento == numDocumento);
+    }
+
+    public async Task<Usuario> GetUsuarioByTelefoneAsync(string telefone)
+    {
+        return await _context.Usuarios.FirstOrDefaultAsync(x => x.Telefone == telefone);
+    }
+
+    public async Task<IEnumerable<Usuario>> GetUsuariosAsync()
+    {
+        return await _context.Usuarios.ToListAsync();
+    }
+
+    public async Task<Usuario> CreateUsuarioAsync(Usuario usuario)
+    {
+        _context.Usuarios.Add(usuario);
+        await _context.SaveChangesAsync();
+        return usuario;
+    }
+
+    public async Task<Usuario> UpdateUsuarioAsync(Usuario usuario)
+    {
+        _context.Entry(usuario).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return usuario;
+    }
+
+    public async Task DeleteUsuarioAsync(int id)
+    {
+        var usuario = await _context.Usuarios.FindAsync(id);
+        _context.Usuarios.Remove(usuario);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ CIDA.Tests/IntegrationAndAbrangenteTests.cs
@@ -0,0 +1,281 @@
+﻿using CIDA.Api.Repository;
+using Cida.Data;
+using CIDA.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CIDA.Tests;
+
+public class IntegrationAndAbrangenteTests : IDisposable
+{
+    private readonly CidaDbContext _context;
+
+    private readonly UsuarioRepository _repository;
+
+    public IntegrationAndAbrangenteTests()
+    {
+        var options = new DbContextOptionsBuilder<CidaDbContext>()
+            .UseInMemoryDatabase("IntegrationAndAbrangenteTests")
+            .Options;
+
+        _context = new CidaDbContext(options);
+        _context.Database.EnsureDeleted();
+
+        _repository = new UsuarioRepository(_context);
+    }
+
+    public void Dispose()
+    {
+        _context.Database.EnsureDeleted();
+        _context.Dispose();
+    }
+
+    [Fact]
+    public async Task CreateUsuarioAsync_ShouldCreateUsuario()
+    {
+        var usuario = new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "12345678901",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        };
+
+        var createdUsuario = await _repository.CreateUsuarioAsync(usuario);
+
+        Assert.NotNull(createdUsuario);
+    }
+
+    [Fact]
+    public async Task GetUsuariosByNomeAsync_ShouldReturnUsuario()
+    {
+        _context.Usuarios.AddRange(Enumerable.Range(10, 5).Select(x => new Usuario
+        {
+            IdUsuario = x,
+            IdAutenticacao = x,
+            Nome = $"Leonardo",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = $"1234567890{x}",
+            Telefone = $"1234567890{x}",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        }));
+
+        _context.Usuarios.Add(new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "(11) 99999-9999",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        });
+
+        await _context.SaveChangesAsync();
+
+        var usuarios = await _repository.GetUsuariosByNomeAsync("Leonardo");
+
+        Assert.NotEmpty(usuarios);
+        Assert.Equal(5, usuarios.Count());
+    }
+
+    [Fact]
+    public async Task GetUsuarioByNumDocumentoAsync_ShouldReturnUsuario()
+    {
+        _context.Usuarios.AddRange(Enumerable.Range(10, 5).Select(x => new Usuario
+        {
+            IdUsuario = x,
+            IdAutenticacao = x,
+            Nome = $"Leonardo",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = $"1234567890{x}",
+            Telefone = $"1234567890{x}",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        }));
+
+        _context.Usuarios.Add(new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "(11) 99999-9999",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        });
+
+        await _context.SaveChangesAsync();
+
+        var usuario = await _repository.GetUsuarioByNumDocumentoAsync("12345678901");
+
+        Assert.NotNull(usuario);
+    }
+
+    [Fact]
+    public async Task GetUsuarioByTelefoneAsync_ShouldReturnUsuario()
+    {
+        _context.Usuarios.AddRange(Enumerable.Range(10, 5).Select(x => new Usuario
+        {
+            IdUsuario = x,
+            IdAutenticacao = x,
+            Nome = $"Leonardo",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = $"1234567890{x}",
+            Telefone = $"1234567890{x}",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        }));
+
+        _context.Usuarios.Add(new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "(11) 99999-9999",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        });
+
+        await _context.SaveChangesAsync();
+
+        var usuario = await _repository.GetUsuarioByTelefoneAsync("(11) 99999-9999");
+
+        Assert.NotNull(usuario);
+    }
+
+    [Fact]
+    public async Task GetUsuariosAsync_ShouldReturnUsuarios()
+    {
+        _context.Usuarios.AddRange(Enumerable.Range(10, 5).Select(x => new Usuario
+        {
+            IdUsuario = x,
+            IdAutenticacao = x,
+            Nome = $"Leonardo",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = $"1234567890{x}",
+            Telefone = $"1234567890{x}",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        }));
+
+        _context.Usuarios.Add(new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "(11) 99999-9999",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        });
+
+        await _context.SaveChangesAsync();
+
+        var usuarios = await _repository.GetUsuariosAsync();
+
+        Assert.NotEmpty(usuarios);
+        Assert.Equal(6, usuarios.Count());
+    }
+
+    [Fact]
+    public async Task UpdateUsuarioAsync_ShouldUpdateUsuario()
+    {
+        var usuario = new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "12345678901",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        };
+
+        var createdUsuario = await _repository.CreateUsuarioAsync(usuario);
+
+        createdUsuario.Nome = "Teste Update";
+
+        var updatedUsuario = await _repository.UpdateUsuarioAsync(createdUsuario);
+
+        Assert.Equal("Teste Update", updatedUsuario.Nome);
+    }
+
+    [Fact]
+    public async Task DeleteUsuarioAsync_ShouldDeleteUsuario()
+    {
+        var usuario = new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "12345678901",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        };
+
+        var createdUsuario = await _repository.CreateUsuarioAsync(usuario);
+
+        await _repository.DeleteUsuarioAsync(createdUsuario.IdUsuario);
+
+        var deletedUsuario = await _repository.GetUsuarioAsync(createdUsuario.IdUsuario);
+
+        Assert.Null(deletedUsuario);
+    }
+
+    [Fact]
+    public async Task GetUsuarioAsync_ShouldReturnUsuario()
+    {
+        var usuario = new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "12345678901",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        };
+
+        var createdUsuario = await _repository.CreateUsuarioAsync(usuario);
+
+        var getUsuario = await _repository.GetUsuarioAsync(createdUsuario.IdUsuario);
+
+        Assert.NotNull(getUsuario);
+    }
+
+    [Fact]
+    public async Task GetUsuarioAsync_ShouldReturnNull()
+    {
+        var usuario = new Usuario
+        {
+            IdUsuario = 1,
+            IdAutenticacao = 1,
+            Nome = "Teste",
+            TipoDocumento = TipoDocumento.CPF,
+            NumDocumento = "12345678901",
+            Telefone = "12345678901",
+            DataCriacao = DateTime.Now,
+            Status = Status.ativo
+        };
+
+        var createdUsuario = await _repository.CreateUsuarioAsync(usuario);
+
+        var getUsuario = await _repository.GetUsuarioAsync(2);
+
+        Assert.Null(getUsuario);
+    }
+}
